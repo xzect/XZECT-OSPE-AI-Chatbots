7ec230b8621ecb7cd56e23481afe7c1fb8db689e
bkraj0304 <raj0304.nit.m@gmail.com>
2024-08-07 23:55:52 +0530

    Resume Builder first commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+// pages/_app.js
+import '../styles/App.css';
+
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ utils/prompts.js
@@ -0,0 +1,13 @@
+export const resumePrompts = [
+    "What is your full name?",
+    "What is your email address?",
+    "What is your phone number?",
+    "Can you provide a brief summary about yourself?",
+    "What are your key skills?",
+    "Please list your work experience.",
+    "Please list your education background.",
+    "Do you have any certifications?",
+    "What are your hobbies or interests?",
+    "Do you have any references?"
+  ];
+  
